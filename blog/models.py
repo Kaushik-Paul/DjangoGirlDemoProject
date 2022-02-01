2f66caa810d6bebe6c036f957b16d7c0e7ef6e4d
kaushikpaul <kaushik.paul755@gmail.com>
2022-02-01 16:41:56 +0530

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    text = models.TextField(blank=True, null=True)
+    created_on = models.DateTimeField(default=timezone.now())
+    published_on = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.title) + " | " + str(self.author)
+
+    def publish(self):
+        self.published_on = timezone.now()
+        self.save()
+
+
