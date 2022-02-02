7ae3093c1d2411f8f712ae3e49c185fa2823c471
kaushikpaul <kaushik.paul755@gmail.com>
2022-02-02 14:59:39 +0530

    Added permission to the api

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('api/', include('blog.api.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
